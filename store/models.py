2f3c46480e553f642d0827417da003f4ae3703a8
Gourav Kumar <gouravcoc667@gmail.com>
2021-10-12 18:17:42 +0530

    Added Book_Copy_Model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class BookCopy(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateField(null=True, blank=True)
+    # Available true means that the copy is available for issue, False means unavailable
+    available = models.BooleanField(default=True)
+    borrower = models.ForeignKey(User, related_name='borrower', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def str(self):
+        if self.available:
+            return f'{self.book.title} - Available'
+        else:
+            return f'{self.book.title}, {str(self.borrow_date)}'
+
