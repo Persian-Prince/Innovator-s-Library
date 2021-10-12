45c37f8912c2f41abe13f757b9ae61e63e6a42f1
mihir02badkur <mihir27badkur@gmail.com>
2021-10-12 15:38:23 +0530

    urls added

--- InnovatorsLibrary/urls.py
+++ InnovatorsLibrary/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('userAccounts/', include('authentication.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
