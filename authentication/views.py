83d7d1389e7bc564a1dae7d7b807a1608bada33d
Jani Paasiluoto <jani.paasiluoto@innokasmedical.fi>
2021-10-13 15:28:09 +0300

    added registerView function

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 # Create your views here.
 
 
@@ -12,4 +14,15 @@ def logoutView(request):
 
 
 def registerView(request):
-    pass
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
