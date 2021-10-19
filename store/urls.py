41c8e67ddaa6a60e3501da4a43e6dab6ff99fafd
ayushigupta931 <ayushigupta931@gmail.com>
2021-10-19 16:18:01 +0530

    store app urls

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from store.views import *
 
 urlpatterns = [
     path('', index, name="index"),
-    
-    
+    path('books/', bookListView, name="book-list"),
+    path('book/<int:bid>/', bookDetailView, name='book-detail' ),
+    path('books/loaned/', viewLoanedBooks, name="view-loaned"),
+    path('books/loan/', loanBookView, name="loan-book"),
+    path('books/return/', returnBookView, name="return-book"),
+    path('books/rate/', rateBookView, name="rate-book"),
 ]
