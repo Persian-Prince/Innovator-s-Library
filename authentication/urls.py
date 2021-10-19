4ddf18e0afc1f199e793c164d1578b031c82c02d
RainaJain5 <rj35100@gmail.com>
2021-10-19 16:36:48 +0530

    authentication app urls added

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,8 @@ from authentication.views import *
 
 urlpatterns = [
     path('login/', loginView, name="login"),
+    path('loggingIn/', loggingIn, name="loggingIn"),
+    path('registerScreen/', registerScreenView, name="registerScreen"),
     path('register/', registerView, name="register"),
 
 ]
